d7067221aafbe7276d2b474e30ecbb06cb7995e9
Jonathan Sun <Jspsun@gmail.com>
2016-11-05 03:19:32 -0400

    Update webhandler.py

--- scripts/webhandler.py
+++ scripts/webhandler.py
@@ -2,11 +2,12 @@ import webbrowser
 
 
 def OpenURL(url):
-    webbrowser.open(url);
-    
-def GoogleSearch(query):
-    webbrowser.open("https://google.ca/#q=" + "+".join(query.strip().split()))
-    
-    
+    webbrowser.open(url)
 
-    
+
+def GoogleSearch(query):
+    webbrowser.open("https://google.ca/#q=" + str(query))
+
+
+def ImFeelingLuckySearch(query):
+    webbrowser.open("")
