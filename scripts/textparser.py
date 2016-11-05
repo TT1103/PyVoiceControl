88a68917f63bf5555e369103557e0fa38823dfaa
Jspsun <Jspsun@gmail.com>
2016-11-05 02:58:51 -0400

    encapsulated files into the text parser

--- scripts/textparser.py
+++ scripts/textparser.py
@@ -1,4 +1,11 @@
+from webhandler import*
 
-#parses the input test and determines a valid command
-def GetCommand(s):
+# parses the input test and determines a valid command
+
+
+def GetCommand(text):
+
+    if(text[0: 6] == "Google"):
+        # run google script
+        GoogleSearch(text[7:])
     pass
